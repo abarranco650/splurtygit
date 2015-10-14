cac721ca13cde13c6908d73a6d5f23066acbf214
Angel <abarranco650@yahoo.com>
2015-10-14 20:56:24 +0000

    Template is missing error when building about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Splurty::Application.routes.draw do
   root 'quotes#index'
-
   resources :quotes
+  get 'about', :to => 'quotes#about'
+
   # The prioroty is based upon order of creation: first created -> highest priority.
   # More comments here ....
 end
